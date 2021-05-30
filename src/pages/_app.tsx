9c199450a163bef7100e82d989f8137e9cd928d8
Gabriel Souza <apgrl123@gmail.com>
2021-05-30 20:16:57 -0300

    first comm

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Header } from '../components/Header';
+import '../styles/global.scss';
+
+
+function MyApp({ Component, pageProps}) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Head from 'next/head'
+export default function Home() {
+  return (
+    <div >
+      <Head>
+        <title>Nome do título</title>
+      </Head>
+      <h1>hello next</h1>
+    </div>
+  )
+}
